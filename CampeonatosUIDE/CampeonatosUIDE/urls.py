a726f8643c83e38168060d6ee45e90e3d00afb2f
Dilan-DS <stgalvezpe@uide.edu.ec>
2025-08-11 09:41:43 -0500

    arreglando error de setting de campeonato

--- CampeonatosUIDE/CampeonatosUIDE/urls.py
+++ CampeonatosUIDE/CampeonatosUIDE/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', include('core.urls')),
+    path("admin/", admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
