32ca2e2e0c2610bcab692b795cbed3115597756d
Dilan-DS <stgalvezpe@uide.edu.ec>
2025-08-11 12:20:43 -0500

    actualizar

--- CampeonatosUIDE/core/views/perfil_views.py
+++ CampeonatosUIDE/core/views/perfil_views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from core.forms import PerfilUsuarioForm
 
 # ========================
 # PERFIL DE USUARIO
@@ -12,5 +14,13 @@ def vista_perfil_usuario(request):
 
 @login_required
 def editar_perfil(request):
-    # Obtiene el usuario autenticado
-    return render(request, 'usuario/editar_perfil.html')
+    user = request.user
+    if request.method == "POST":
+        form = PerfilUsuarioForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Perfil actualizado correctamente.")
+            return redirect("perfil_usuario")
+    else:
+        form = PerfilUsuarioForm(instance=user)
+    return render(request, "usuario/editar_perfil.html", {"form": form})
